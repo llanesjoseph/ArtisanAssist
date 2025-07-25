cf414550d90bc7220d645b23b13224765de376e1
Joseph Llanes <llanes.joseph.m@gmail.com>
2025-07-25 13:13:15 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,15 +1,19 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-    const serviceAccount = JSON.parse(
-        process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-    );
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-    });
+    if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+        const serviceAccount = JSON.parse(
+            process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+        );
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+        });
+    } else {
+        console.warn("FIREBASE_SERVICE_ACCOUNT_KEY is not set. Firebase Admin SDK will not be initialized.");
+    }
 }
 
-const db = admin.firestore();
-const auth = admin.auth();
+const db = admin.apps.length ? admin.firestore() : null;
+const auth = admin.apps.length ? admin.auth() : null;
 
 export { db, auth };
